6173b0c77af82e16fd56be3f4e991feb3fe05182
Carla <carla.debeer.uk@gmail.com>
2017-03-01 09:23:30 +0000

    Adding The Matrix digital rain code

---
+++ matrixDigitalRain/scriptFiles/symbol.js
@@ -0,0 +1,26 @@
+function Symbol(x, y, speed, first) {
+
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
+	this.switchInterval = round(random(2, 10));
+	this.first = first;
+
+	this.setToRandomSymbol = function() {
+		if (frameCount % this.switchInterval === 0) {
+			var charValue = round(random(0, 192));
+			//this.value = String.fromCharCode(0x30A0 + round(random(0, 96)); 	// Katakana
+			//this.value = String.fromCharCode(0x0400 + round(random(0, 256)); 	// Cyrillic
+			//this.value = String.fromCharCode(0x0600 + round(random(0, 255)); 	// Arabic
+			//this.value = String.fromCharCode(0x2190 + round(random(0, 112)); 	// Arrows
+			this.value = String.fromCharCode(0x2700 + charValue); 	    		// Dingbats
+			charValue === 118 || charValue === 119 || charValue === 120 || charValue === 121|| charValue === 122 ||
+			charValue === 123 || charValue === 124 || charValue === 125 || charValue === 126 || charValue === 127
+				? this.special = true : this.special = false;
+		}
+	};
+
+	this.rain = function() {
+		this.y = (this.y >= height) ? 0 : this.y += this.speed;
+	};
+}
---
+++ matrixDigitalRain/sketch.js
@@ -0,0 +1,27 @@
+// Carla de Beer
+// March 2017
+// Simulation of The Matrix's Digital Rain.
+// Based on Emily Xie's Coding Train video example.
+
+var streams = [];
+var symbolSize = 25;
+
+function setup() {
+	createCanvas(window.innerWidth, window.innerHeight);
+	textSize(symbolSize);
+
+	var x = 0;
+	for(var i = 0; i <= width/symbolSize; ++i) {
+		var stream = new Stream();
+		stream.generateSymbols(x, round(random(-1000, 0)));
+		streams.push(stream);
+		x += symbolSize;
+	}
+}
+
+function draw() {
+	background(0, 128);
+	streams.forEach(function(stream){
+		stream.render();
+	});
+}
