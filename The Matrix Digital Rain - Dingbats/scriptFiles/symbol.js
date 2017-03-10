2c020a7bf3bc3c60ff0bc6d7e03ac19f3468aadb
Carla <carla.debeer.uk@gmail.com>
2017-03-10 07:16:52 +0000

    Minor updates to simplify code base

--- The Matrix Digital Rain - Dingbats/scriptFiles/symbol.js
+++ The Matrix Digital Rain - Dingbats/scriptFiles/symbol.js
@@ -7,16 +7,12 @@ function Symbol(x, y, speed, first) {
 
 	this.setToRandomSymbol = function() {
 		if (frameCount % this.switchInterval === 0) {
+			// Load Dingbats font
 			var charValue = round(random(0, 192));
-			//this.value = String.fromCharCode(0x30A0 + round(random(0, 96))); 	// Katakana
-			//this.value = String.fromCharCode(0x0400 + round(random(0, 256))); // Cyrillic
-			//this.value = String.fromCharCode(0x0600 + round(random(0, 255))); // Arabic
-			//this.value = String.fromCharCode(0x2190 + round(random(0, 112))); // Arrows
-			this.value = String.fromCharCode(0x2700 + charValue); 	    		// Dingbats
-			charValue === 118 || charValue === 119 || charValue === 120 || charValue === 121 || charValue === 122 ||
-			charValue === 123 || charValue === 124 || charValue === 125 || charValue === 126 || charValue === 127 ||
-			charValue === 138 || charValue === 139 || charValue === 140 || charValue === 141 || charValue === 142 ||
-			charValue === 143 || charValue === 144 || charValue === 145 || charValue === 146 || charValue === 147
+			this.value = String.fromCharCode(0x2700 + charValue);
+
+			// Highlight the solid fill number values in a reddish colour
+			charValue >= 118 && charValue <= 127 || charValue >= 138 && charValue <= 147
 				? this.special = true : this.special = false;
 		}
 	};
