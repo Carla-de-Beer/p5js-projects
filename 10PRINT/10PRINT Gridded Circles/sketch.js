7dacf718f476d9f87757196294dcd9f1430a432c
Carla <carla.debeer.uk@gmail.com>
2017-10-14 21:11:19 +0100

    Adding 10PRINT Gridded Circles

---
+++ 10PRINT/10PRINT Gridded Circles/sketch.js
@@ -0,0 +1,47 @@
+// Carla de Beer
+// October 2017
+// Gridded circles, based on the 10PRINT algorithm.
+
+// Based on Daniel Shiffman's Coding Train video example:
+// https://www.youtube.com/watch?v=bEyTZ5ZZxZs
+
+let spacing = 40;
+let x = spacing/2;
+let y = spacing/2;
+
+function setup() {
+	createCanvas(1320, 640);
+	background(55);
+}
+
+function draw() {
+	let w = random(1, 8);
+
+	let r1 = random(15, 200);
+	let r2 = random(15, 200);
+
+	let g1 = random(15, 200);
+	let g2 = random(15, 200);
+
+	let b1 = random(15, 200);
+	let b2 = random(15, 200);
+
+	if (random(1) < 0.5) {
+        stroke(r1, g1, b1);
+		strokeWeight(w);
+		fill(r2, g2, b2);
+		ellipse(x, y, 20);
+	}
+
+	x += spacing;
+
+	if (x >= width) {
+		x = spacing/2;
+		y += spacing;
+	}
+
+	if (y >= height) {
+		noLoop();
+	}
+}
+
