4086b101a48efd4fc5524125e99dfac07045f131
Carla <carla.debeer.uk@gmail.com>
2017-11-12 12:24:24 +0000

    Add Circle Morph Steer code

---
+++ Circle Morphing/Circle Morph Steer/particle.js
@@ -0,0 +1,52 @@
+
+class Particle {
+  constructor(pos, node) {
+    this.pos = pos;
+    this.acc = new p5.Vector(0, 0);
+    this.vel = new p5.Vector(0, 0);
+		this.node = node;
+  }
+
+  run() {
+    // Update velocity
+    this.vel.add(this.acc);
+    // Limit speed
+    this.vel.limit(MAX_SPEED);
+    this.pos.add(this.vel);
+    // Reset accelertion to 0 each cycle
+    this.acc.mult(0);
+  }
+
+  drawParticle() {
+    noStroke();
+    fill(237, 34, 93);
+    ellipse(this.pos.x, this.pos.y, 4);
+		noFill();
+  }
+
+  arrive(target) {
+    this.acc.add(this.steer(target, true));
+  }
+
+  // A method that calculates a steering vector towards a target
+  // Takes a second argument, if true, it slows down as it approaches the target
+  steer(target, slowdown) {
+    let steer;
+    let desired = p5.Vector.sub(target, this.pos);  // A vector pointing from the location to the target
+    let d = desired.mag(); // Distance from the target is the magnitude of the vector
+    // If the distance is greater than 0, calc steering (otherwise return zero vector)
+    if (d > 0) {
+      desired.normalize();
+      if ((slowdown) && (d < 100.0)) {
+        desired.mult(MAX_SPEED*(d/100.0)); // This damping is somewhat arbitrary
+      } else {
+        desired.mult(MAX_SPEED);
+      }
+      steer = p5.Vector.sub(desired, this.vel);
+      steer.limit(MAX_FORCE);  // Limit to maximum steering force
+    } else {
+      steer = new p5.Vector(0, 0);
+    }
+    return steer;
+  }
+}
---
+++ Circle Morphing/Circle Morph Steer/sketch.js
@@ -0,0 +1,143 @@
+// Carla de Beer
+// November 2017
+// Circle morphing (morphing a circle into a square, and back again, by means of steering).
+
+// Steer and arrive alogorithms based on those by Daniel Shiffman ("Nature of Code").
+
+let cirPath = [];
+let sqrPath = [];
+let particles = [];
+let spacing = 2;
+let MAX_SPEED = 1;
+let MAX_FORCE = 0.008;
+
+let checkBox;
+
+let isReversed = false;
+let isDebug = true;
+
+function setup() {
+	createCanvas(500, 500);
+	angleMode(DEGREES);
+  resetSketch();
+	checkBox = createCheckbox(" " + "debug", true);
+}
+
+function draw() {
+	background(220);
+
+	isDebug = checkBox.checked();
+
+	push();
+	translate(width/2, height/2);
+	rotate(45);
+	stroke(51);
+	strokeWeight(1);
+	noFill();
+
+	beginShape();
+	for (let i = 0; i < particles.length; ++i) {
+			vertex(particles[i].pos.x, particles[i].pos.y);
+	}
+	endShape(CLOSE);
+
+	if (isReversed) {
+		if (isDebug) {
+			for (let i = 0; i < particles.length; ++i) {
+				particles[i].drawParticle();
+			}
+		}
+
+		for (let i = 0; i < particles.length; ++i) {
+				particles[i].arrive(cirPath[i]);
+				particles[i].run();
+		}
+	} else {
+			if (isDebug) {
+				for (let i = 0; i < particles.length; ++i) {
+					particles[i].drawParticle();
+				}
+			}
+
+			for (let i = 0; i < particles.length; ++i) {
+				if (particles[i].node === 0) {
+					particles[i].arrive(sqrPath[0]);
+					particles[i].run();
+				} else if (particles[i].node === 1) {
+					particles[i].arrive(sqrPath[1]);
+					particles[i].run();
+				} else if (particles[i].node === 2) {
+					particles[i].arrive(sqrPath[2]);
+					particles[i].run();
+				} else if (particles[i].node === 3) {
+					particles[i].arrive(sqrPath[3]);
+					particles[i].run();
+				}
+			}
+		}
+
+	for (let i = 0; i < particles.length; ++i) {
+		if (frameCount % 800 === 0) {
+			console.log("Reverse!");
+			isReversed = !isReversed;
+			resetSketch();
+		}
+	}
+
+	pop();
+}
+
+function polarToCartesian(r, a) {
+	return createVector(r*cos(a), r*sin(a));
+}
+
+function addVectors() {
+	let radius = 200;
+	for (let angle = 0; angle <= 360; angle += spacing) {
+		let cv = polarToCartesian(radius, angle);
+		if (angle <= 90) {
+			cv.node = 0;
+		} else if (angle > 90 && angle <= 180) {
+			cv.node = 1;
+		} else if (angle > 180 && angle <= 270) {
+			cv.node = 2;
+		} else if (angle > 270) {
+			cv.node = 3;
+		}
+
+		// Add to circle path
+		cirPath.push(cv);
+
+		// Add to square path
+		if (angle % 90 === 0 && angle > 0 && angle <= 360) {
+			sqrPath.push(cv);
+		}
+	}
+}
+
+function resetSketch() {
+	cirPath = [];
+	sqrPath = [];
+	particles = [];
+	addVectors();
+
+	if (isReversed) {
+		for (let i = 0; i < cirPath.length; ++i) {
+			let cv;
+			if (cirPath[i].node === 0) {
+				cv = createVector(sqrPath[0].x, sqrPath[0].y);
+			} else if (cirPath[i].node === 1) {
+				cv = createVector(sqrPath[1].x, sqrPath[1].y);
+			} else if (cirPath[i].node === 2) {
+				cv = createVector(sqrPath[2].x, sqrPath[2].y);
+			} else {
+				cv = createVector(sqrPath[3].x, sqrPath[3].y);
+			}
+			particles.push(new Particle(cv, cirPath[i].node));
+		}
+	} else {
+		for (let i = 0; i < cirPath.length; ++i) {
+			particles.push(new Particle(cirPath[i], cirPath[i].node));
+		}
+	}
+}
